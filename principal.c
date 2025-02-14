6316e263bbf2d0cd6eb7086e32efaa146bb01397
John Miguel <jonhmiguel132@gmail.com>
2025-02-14 17:04:49 -0300

    Tratando erro de entrada de string na variável de'opção'

--- principal.c
+++ principal.c
@@ -143,42 +143,47 @@ void reorganizar_pecas() {
 
 
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(){
-    //Representação do menu de usuário:
+    // Representação do menu de usuário:
     int opcao;
     
-    do{
-    printf("\033[1;31mMenu:\033[0m\n");
-    printf("\033[1;35m_______________________\033[0m\n");
-    printf("\033[1;31m1 - Cadastrar peça\033[0m\n");
+    do {
+        printf("\033[1;31mMenu:\033[0m\n");
+        printf("\033[1;35m_______________________\033[0m\n");
+        printf("\033[1;31m1 - Cadastrar peça\033[0m\n");
+        printf("\033[1;35m_______________________\033[0m\n");
+        printf("\033[1;31m2 - Listar peças\033[0m\n");
+        printf("\033[1;35m_______________________\033[0m\n");
+        printf("\033[1;31m3 - Reorganizar peças\033[0m\n");
+        printf("\033[1;35m_______________________\033[0m\n");
+        printf("\033[1;31m0 - Sair\033[0m\n");
+        printf("Opção: ");
+        
+        if (scanf("%d", &opcao) != 1) { //Tratando erro para caso o usuário insira uma string.
+            printf("Entrada inválida, insira um número válido.\n");
+            while (getchar() != '\n');  
+            continue;  
+        }
 
-    printf("\033[1;35m_______________________\033[0m\n");
-    printf("\033[1;31m2 - Listar peças\033[0m\n");
-
-    printf("\033[1;35m_______________________\033[0m\n");
-    printf("\033[1;31m3 - Reorganizar peças\033[0m\n");
-    
-    printf("\033[1;35m_______________________\033[0m\n");
-    printf("\033[1;31m0 - Sair\033[0m\n");
-    printf("Opção: ");
-    scanf("%d", &opcao);
-
-        switch (opcao){
-        case 1:
-            cadastro();
-            break;
-        case 2:
-            listar_pecas();
-            break;
-        case 3:
-            reorganizar_pecas();
-            break;
-        case 0:
-            printf("Programa encerrado com sucesso\n");
-            break;
-        default:
-        printf("Opção não encontrada, tente novamente.\n");
-            break;
+        switch (opcao) {
+            case 1: 
+                cadastro(); 
+                break;
+            case 2:
+                listar_pecas();  
+                break;
+            case 3:
+                reorganizar_pecas();  
+                break;
+            case 0:
+                printf("Programa encerrado com sucesso\n");
+                break;
+            default:
+                printf("Opção não encontrada, tente novamente.\n");
+                break;
         }
     } while (opcao != 0);
     
